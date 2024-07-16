1e59952e5208aa94aff63770c196c403d4c6f8e7
Chris Adkin <chris.adkin@hashicorp.com>
2024-07-16 18:41:08 +0100

    Update main.tf
    vpc_zone_identifier removed from aws_autoscaling_group resource

--- modules/nomad_cluster/main.tf
+++ modules/nomad_cluster/main.tf
@@ -53,7 +53,6 @@ resource "aws_autoscaling_group" "nomad_servers" {
   wait_for_capacity_timeout = "480s"
   health_check_grace_period = 15
   health_check_type         = "EC2"
-  vpc_zone_identifier       = data.aws_subnets.default.ids
   
   tag {
     key                 = "Name"
